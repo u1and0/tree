2e440127c5c919ad686f69493f3f0c4b22371336
u1and0 <e01.ando60@gmail.com>
2022-03-05 15:06:38 +0900

    [mod] entries を辞書順ソート

--- tree.ts
+++ tree.ts
@@ -1,9 +1,27 @@
+// vim: set shiftwidth=2:
 import { join, resolve } from "https://deno.land/std@0.100.0/path/mod.ts";
 
+// 連結したパスをオブジクトに含め、配列の順序を辞書順に直しておく
+export interface TreeEntry extends Deno.DirEntry{
+    path:string;
+}
+
 const tree = async (root: string) => {
+  const entries: TreeEntry[] = [];
   for await (const entry of Deno.readDir(root)) {
-      // readDirでは一階層したのファイルパスだけ
-    console.log(join(root, entry.name));
+    // readDirでは一階層したのファイルパスだけ
+    // console.log(join(root, entry.name));
+    const treeEntry = { ...entry, path: join(root, entry.name) }
+    //                  ^^^ ...entryとは？
+    entries.push(treeEntry)
+  }
+
+  const sortedEntries = entries.sort((a,b)=>
+    a.name.toLowerCase() > b.name.toLowerCase() ? 1:-1
+  );
+
+  for await (const entry of sortedEntries) {
+    console.log(entry.path)
   }
 };
 
