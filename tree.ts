2409794ef61afd4cdfe0e0f171f57f8ce7807a73
u1and0 <e01.ando60@gmail.com>
2022-03-05 17:32:19 +0900

    [mod] tree 表示の変更

--- tree.ts
+++ tree.ts
@@ -1,19 +1,54 @@
 // vim: set shiftwidth=2:
 import { join, resolve } from "https://deno.land/std@0.100.0/path/mod.ts";
+import {parse } from "https://deno.land/std@0.100.0/flags/mod.ts";
+
+// tree 表示を調整するオプションのインタ―フェース
+export interface TreeOptions{
+  maxDepth?: number // -L
+  includeFiles?: boolean // -d
+  skip?: RegExp[] // -a -u
+}
+
+function include( path: string, skip?: RegExp[]): boolean{
+  // skipには-aおよび-uオプションの有無に応じて
+  // スキップ対象の正規表を格納し
+  // それとマッチした場合表示をスキップ
+  if (skip && skip.some((pattern): boolean => !!path.match(pattern))){
+    return false
+  }
+  return true
+}
 
 // 連結したパスをオブジクトに含め、配列の順序を辞書順に直しておく
 export interface TreeEntry extends Deno.DirEntry {
   path: string;
 }
 
-const tree = async (root: string, prefix = "") => {
+const tree = async (
+  root: string,
+  prefix = "", 
+  {
+    maxDepth=Infinity,
+    includeFiles = true,
+    skip = undefined,
+  }: TreeOptions = {},
+  ) => {
+  // depth が0またはスキップ対象なら終了
+  if (maxDepth < 1 || !include(root, skip)){
+    return;
+  }
+
   const entries: TreeEntry[] = [];
   for await (const entry of Deno.readDir(root)) {
     // readDirでは一階層したのファイルパスだけ
     // console.log(join(root, entry.name));
-    const treeEntry = { ...entry, path: join(root, entry.name) };
+    // const treeEntry = { ...entry, path: join(root, entry.name) };
     //                  ^^^ ...entryとは？
-    entries.push(treeEntry);
+    if ( entry.isFile && !includeFiles ){
+      continue;
+    }
+    // entries.push(treeEntry);
+    entries.push( { ...entry, path: join(root, entry.name) });
   }
 
   if (entries.length == 0) {
@@ -27,17 +62,25 @@ const tree = async (root: string, prefix = "") => {
 
   for await (const entry of sortedEntries) {
     const branch = entry === lastOne ? "└── " : "├── "; // ディレクトリ内の最後の項目だったら左の枝
+    const suffix = (entry.isDirectory) ? "/" : ""; // ディレクトリなら末尾に"/"
 
     // console.log(prefix + entry.path)
     // console.log(prefix + entry.name)  // フルパスではなく、ファイル名だけ表示
-    console.log(prefix + branch + entry.name);
+    // console.log(prefix + branch + entry.name);
+    if (include(entry.path, skip)){
+      console.log(prefix + branch + entry.name + suffix);
+    }
 
     // entryがディレクトリだったら再帰的にtree呼び出し
-    if (entry.isDirectory && entry.name !== ".git") { // ignore .git directory
+    if (entry.isDirectory) { // ignore .git directory
       // await tree(entry.path)
       // await tree(entry.path, prefix+"  ")  // 再帰的処理をするときに、prefixのスペースが増えていく
       const indent = entry === lastOne ? "  " : "| ";  // 再帰処理をするときにディレクトリ内の最後の項目だったら左のindent
-      await tree(entry.path, prefix + indent);
+      await tree(entry.path, prefix + indent, {
+        maxDepth: maxDepth-1,
+        includeFiles,
+        skip,
+      });
     }
   }
 };
