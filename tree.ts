b4b8f041dc9bdcc392dd3ebb016f6c43e29a7e41
u1and0 <e01.ando60@gmail.com>
2022-03-05 15:20:58 +0900

    [add] branch string

--- tree.ts
+++ tree.ts
@@ -16,13 +16,22 @@ const tree = async (root: string, prefix="") => {
     entries.push(treeEntry)
   }
 
+  if (entries.length == 0){
+    return;
+  }
+
   const sortedEntries = entries.sort((a,b)=>
     a.name.toLowerCase() > b.name.toLowerCase() ? 1:-1
   );
+  const lastOne = sortedEntries[entries.length - 1]
 
   for await (const entry of sortedEntries) {
+    const branch = entry === lastOne ?  "└── " : "├── ";  // ディレクトリ内の最後の項目だったら左の枝
+
     // console.log(prefix + entry.path)
-    console.log(prefix + entry.name)  // フルパスではなく、ファイル名だけ表示
+    // console.log(prefix + entry.name)  // フルパスではなく、ファイル名だけ表示
+    console.log(prefix + branch+entry.name)
+
     // entryがディレクトリだったら再帰的にtree呼び出し
     if (entry.isDirectory && entry.name !== ".git"){ // ignore .git directory
       // await tree(entry.path)
